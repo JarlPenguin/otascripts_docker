3f8a5a518ceea580df7a5ad4b0e86a40a4dbb65e
JarlPenguin <jarlpenguin@outlook.com>
2019-07-26 21:02:32 +0300

    Simplify script for otascripts
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

--- build.sh
+++ build.sh
@@ -1,13 +1,5 @@
 apt update
 apt upgrade -y
-apt install sudo git lsb-release apt-utils p7zip-full wget curl brotli rsync -y
-git config --global user.email "baalajimaestro@raphielgang.org"
-git config --global user.name "baalajimaestro"
-git clone https://github.com/akhilnarang/scripts
-cd scripts
-bash setup/android_build_env.sh
-unlink /usr/bin/python
-ln -s /lib/x86_64-linux-gnu/libncurses.so.6 /usr/lib/x86_64-linux-gnu/libncurses.so.5
-ln -s /usr/bin/python2.7 /usr/bin/python
-cd ..
-rm -rf scripts
+apt install sudo git lsb-release curl unzip sed -y
+git config --global user.email "jarlpenguin@outlook.com"
+git config --global user.name "Jarl-Penguin"
